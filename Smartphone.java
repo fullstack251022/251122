4ba29c56233be3fadcd7476cc1092ad0c4016ac7
fullstack251022 <116830126+fullstack251022@users.noreply.github.com>
2022-11-25 12:59:33 +0200

    Create Smartphone.java

---
+++ Smartphone.java
@@ -0,0 +1,21 @@
+public class Smartphone {
+
+    public String type;
+    public String model;
+    public float screenSize;
+    public String screenType;
+    public float cameraPixels;
+    public int yearOfProduction;
+
+    public Smartphone(String type, String model, float screenSize, String screenType,float cameraPixels, int yearOfProduction ) {
+        this.screenSize = screenSize;
+        this.type = type;
+        this.model = model;
+        this.screenType = screenType;
+        this.cameraPixels = cameraPixels;
+        this.yearOfProduction = yearOfProduction;
+    }
+
+
+    public String color;
+}
